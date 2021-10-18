cfeb9c5a997b8ebf19fab7fb5db584e7cc7574f2
YanaGorovets <92453208+YanaGorovets@users.noreply.github.com>
2021-10-18 16:44:42 +0300

    Add files via upload

---
+++ polymorphism/polymorphism/Properties/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism.Properties
+{
+    public class ClassA
+    {
+        public virtual void XXX()
+        {
+            Console.WriteLine("ClassA XXX");
+        }
+    }
+    public abstract class ClassB:ClassA
+    {
+        public new abstract void XXX();
+    }
+    public class ClassC:ClassB
+    {
+        public override void XXX()
+        {
+            System.Console.WriteLine("ClassC XXX");
+        }
+    }
+    public class Program
+    {
+        private static void Main(string [] args)
+        {
+            ClassA classA = new ClassC();
+            ClassB classB = new ClassC();
+            classA.XXX();
+            classB.XXX();
+        }
+    }
+}
