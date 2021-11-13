fed300cbb7db3a0a843472cb63ac96dc6d6fc3f0
YanaGorovets <92453208+YanaGorovets@users.noreply.github.com>
2021-11-13 19:17:32 +0300

    Add files via upload

---
+++ AQA 04 2/AQA 04 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AQA_04_2;
+namespace AQA_04_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(6);
+            numbers.Enqueue(3);
+            numbers.Enqueue(14);
+            numbers.Enqueue(45);
+            numbers.Enqueue(100);
+            numbers.Enqueue(15);
+            numbers.Enqueue(49);
+            Console.WriteLine(" Queue:");
+            foreach(int num in numbers)
+            {
+                Console.Write(" " + num);
+            }
+            int min = numbers.Peek();
+            int max = numbers.Peek();
+            int sum = 0;
+            foreach (int i in numbers)
+            {
+                if ( min> i)
+                    min = i;
+                if (max<i)
+                    max=i;
+            }
+            Console.WriteLine("\n min element: " + min + "\n max element: " + max);
+            Boolean flag = false;
+            foreach (int i in numbers)
+            {
+                if(i == min) 
+                    flag = true;
+                if (flag == true)
+                    sum += i;
+                if (i == max)
+                    flag= false;
+            }
+            Console.WriteLine("sum element: " + sum);
+        }
+    }
+}
