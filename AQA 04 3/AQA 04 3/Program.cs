faab88742f2dd9c77f6ef1aae609be84991e381c
YanaGorovets <92453208+YanaGorovets@users.noreply.github.com>
2021-11-13 19:18:18 +0300

    Add files via upload

---
+++ AQA 04 3/AQA 04 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace SimpleAlgorithmsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("N man:");
+            PrintList(list);
+            bool delete = false;
+            while (list.Count > 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (delete) list.RemoveAt(i--);
+                    delete = !delete;
+                }
+                PrintList(list);
+            }
+            Console.Read();
+
+        }
+        static void PrintList(IEnumerable<int> list)
+        {
+            Console.WriteLine("iteration with deletion:");
+            foreach (var i in list)
+                Console.Write("{0} ", i);
+            Console.WriteLine();
+        }
+    }
+}
