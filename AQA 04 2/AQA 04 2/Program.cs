b90f272804b0e1aeebbdf532aac4b4a90baee28b
YanaGorovets <92453208+YanaGorovets@users.noreply.github.com>
2021-11-21 20:17:09 +0300

    Add files via upload

---
+++ AQA 04 2/AQA 04 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AQA_04_2;
+namespace AQA_04_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(8);
+            numbers.Enqueue(3);
+            numbers.Enqueue(88);
+            numbers.Enqueue(76);
+            numbers.Enqueue(100);
+            numbers.Enqueue(15);
+            numbers.Enqueue(49);
+            Console.WriteLine(" Queue:");
+            foreach(int num in numbers)
+            {
+                Console.Write(" " + num);
+            }
+            int min = numbers.Peek();
+            int max = numbers.Peek();
+            int sum = 0;
+            foreach (int i in numbers)
+            {
+                if ( min> i)
+                    min = i;
+                if (max<i)
+                    max=i;
+            }
+            Console.WriteLine("\n min element: " + min + "\n max element: " + max);
+            Boolean flag = false;
+            Boolean minToMax = false;
+            foreach (int i in numbers)
+            {
+                if (!flag && i == min)
+                {
+                    flag = true;
+                    minToMax = true;
+                }
+                else if (!flag && i == max)
+                    flag = true;
+                if (flag == true)
+                    sum += i;
+                if (minToMax)
+                {
+                    if (i == max)
+                        flag = false;
+                }
+                else if (i == min)
+                    flag = false;
+            }
+            Console.WriteLine("sum element: " + sum);
+        }
+    }
+}
